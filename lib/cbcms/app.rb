74ceeb9491870a6e62f30b73e901b0f1da4b5b72
TADA Tadashi <t@tdtds.jp>
2010-07-23 21:27:26 +0900

    replaced dead copy to making symlink.

--- lib/cbcms/app.rb
+++ lib/cbcms/app.rb
@@ -7,6 +7,9 @@
 require 'pathname'
 
 module CBCMS
+	class CBCMSError < ::StandardError; end
+	class MakeLink < CBCMSError; end
+
 	class App
 		def initialize( src, dest )
 			@src, @dest = src, dest
@@ -20,18 +23,22 @@ module CBCMS
 					processing( pathname, Pathname::new( d ), :template )
 				else
 					d = @dest + pathname.to_s[@src.size,pathname.to_s.size]
-					processing( pathname, Pathname::new( d ), :dead_copy )
+					processing( pathname, Pathname::new( d ), :symlink )
 				end
 			end
 		end
 
 		def processing( src, dest, filter )
 			result = ''
-			File::open( src, 'r:utf-8' ) do |f|
-				result = __send__( filter, f.read )
-			end
-			File::open( dest, 'w' ) do |f|
-				f.write( result )
+			begin
+				File::open( src, 'r:utf-8' ) do |f|
+					result = __send__( filter, f.read )
+				end
+				File::open( dest, 'w' ) do |f|
+					f.write( result )
+				end
+			rescue MakeLink
+				dest.make_symlink( src )
 			end
 		end
 
@@ -46,6 +53,10 @@ module CBCMS
 
 		def dead_copy( tmpl )
 			tmpl
+		end
+
+		def symlink( tmpl )
+			raise MakeLink::new
 		end
 	end
 end
